d87e80960d4721d2b05d65cbcd3cfa54fa4f13a3
Julien Nguyen <junguyen@f6r13s3.clusters.42paris.fr>
2025-06-09 14:31:15 +0200

    modify phonebook.hpp

--- cpp00/ex01/incs/PhoneBook.hpp
+++ cpp00/ex01/incs/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: junguyen <junguyen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 12:44:02 by junguyen          #+#    #+#             */
-/*   Updated: 2025/06/09 12:19:58 by junguyen         ###   ########.fr       */
+/*   Updated: 2025/06/09 14:20:31 by junguyen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,6 @@ class PhoneBook
 		~PhoneBook(void);
 		void	add_contact(std::string info[5]);
 		void	search_contact(void) const;
-		// void	search_index(std::string index) const;
 };
 
 #endif
