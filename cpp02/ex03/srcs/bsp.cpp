8c5a00054db55accbb40fb99c230731aa5402378
Julien Nguyen <junguyen@f1r6s3.clusters.42paris.fr>
2025-06-16 15:19:52 +0200

    ready to push 02

--- cpp02/ex03/srcs/bsp.cpp
+++ cpp02/ex03/srcs/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: junguyen <junguyen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/21 14:24:34 by junguyen          #+#    #+#             */
-/*   Updated: 2025/05/21 16:45:27 by junguyen         ###   ########.fr       */
+/*   Updated: 2025/06/16 12:04:59 by junguyen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,8 @@ bool bsp (Point const a, Point const b, Point const c, Point const point)
     d2 = sign(point, b, c);
     d3 = sign(point, c, a);
 
-    has_neg = (d1 < 0) || (d2 < 0) || (d3 < 0);
-    has_pos = (d1 > 0) || (d2 > 0) || (d3 > 0);
+    has_neg = (d1 <= 0) || (d2 <= 0) || (d3 <= 0);
+    has_pos = (d1 >= 0) || (d2 >= 0) || (d3 >= 0);
 
     return !(has_neg && has_pos);
 }
