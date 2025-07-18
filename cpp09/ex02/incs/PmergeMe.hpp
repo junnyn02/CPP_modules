bdec3652c4f2fe68d15be0367e4a984a26a48410
Julien Nguyen <junguyen@f4r2s7.clusters.42paris.fr>
2025-07-18 18:53:12 +0200

    some test to fix insertion order according to real algo from wiki

--- cpp09/ex02/incs/PmergeMe.hpp
+++ cpp09/ex02/incs/PmergeMe.hpp
@@ -7,44 +7,40 @@
 # include <limits>
 # include <cerrno>
 # include <vector>
+# include <list>
 # include <ctime>
-
-// class PmergeMe {
-// public:
-//     std::vector<int> sort(std::vector<int> &array);
-
-// private:
-//     int binarySearch(std::vector<int> const &S, int n, int maxPos) const;
-//     std::vector<int> generateJacobsthal(int maxIndex) const;
-//     std::vector<int> generateInsertionOrder(int numElements) const;
-//     void insert(std::vector<std::pair<int, int> > const &pairs, std::vector<int> const &S, std::vector<int> &result) const;
-// };
 
 class PmergeMe
 {
 	private:
 		std::vector<int>	_vector;
+		std::list<int>		_list;
 
 		PmergeMe(void);
 		PmergeMe(PmergeMe const &cpy);
 		PmergeMe	&operator=(PmergeMe const &assign);
+		
+		bool				onlyDigit(const char *) const;
 
 		//vector algo
 		std::vector<int>	sort(std::vector<int> &);
-		// std::vector<int>    generateJacobsthal(int maxIndex) const;
-		// std::vector<int>    generateInsertionOrder(int numElements) const;
-		int binarySearch(std::vector<int> const &S, int n, int maxPos) const;
-    std::vector<int> generateJacobsthal(int maxIndex) const;
-    std::vector<int> generateInsertionOrder(int numElements) const;
-    void insert(std::vector<std::pair<int, int> > const &pairs, std::vector<int> const &S, std::vector<int> &result) const;
-		// void				insert(std::vector<int> const &, std::vector<int> const &, std::vector<int> &) const;
-		bool				onlyDigit(const char *) const;
-		// int					binarySearch(std::vector<int> const &S, int n, int maxPos) const;
-		//vector algo
+    	std::vector<int> 	generateJacobsthal(int maxIndex) const;
+    	std::vector<int> 	generateInsertionOrder(int numElements) const;
+    	void 				insert(std::vector<std::pair<int, int> > const &pairs, std::vector<int> const &S, std::vector<int> &result) const;
+		int 				binarySearch(std::vector<int> const &S, int n, int maxPos) const;
 
+		//list algo
+		std::list<int>	sort(std::list<int> &);
+		void 			insert(std::list<std::pair<int, int> > &pairs, std::list<int> const &S, std::list<int> &result) const;
+    	std::list<int> 	generateJacobsthalList(int maxIndex) const;
+    	std::list<int> 	generateInsertionOrderList(int numElements) const;
+		int				binarySearch(std::list<int> const &S, int n, int maxPos) const;		
+		
 	public:
 		PmergeMe(char **, int &);
 		~PmergeMe(void);
+
+		bool	checkSort(void);
 };
 
 # endif
--- cpp09/ex02/srcs/PmergeMe.cpp
+++ cpp09/ex02/srcs/PmergeMe.cpp
@@ -24,15 +24,18 @@ PmergeMe::PmergeMe(char **av, int &ac)
     for (size_t i = 0; i < _vector.size(); i++)
         std::cout << _vector[i] << " ";
     std::cout << std::endl;
-    std::clock_t    start = std::clock();
+    std::clock_t    startV = std::clock();
+    size_t size = _vector.size();
     _vector = sort(_vector);
-    std::clock_t    end = std::clock();
-    double  time = static_cast<double>(end - start) / CLOCKS_PER_SEC;
+    std::clock_t    endV = std::clock();
+    double  timeV = static_cast<double>(endV - startV) / CLOCKS_PER_SEC;
     std::cout << "After:\t";
     for (size_t i = 0; i < _vector.size(); i++)
         std::cout << _vector[i] << " ";
     std::cout << std::endl;
-    std::cout << "Time to process a range of " << _vector.size() << " elements with std::vector :" << time << " us" << std::endl;
+    std::cout << "Time to process a range of " << _vector.size() << " elements with std::vector :" << timeV << " us" << std::endl;
+    if (!checkSort() || size != _vector.size())
+        throw(std::runtime_error("Error: not sorted"));
 }
 
 PmergeMe::~PmergeMe(void)
@@ -50,302 +53,279 @@ PmergeMe &PmergeMe::operator=(PmergeMe const &assign)
     return *this;
 }
 
-// int    PmergeMe::binarySearch(std::vector<int> const &S, int n, int maxPos) const
-// {
-//     int left = 0;
-//     int right = std::min(maxPos, (int)S.size() - 1);
-//     while(left <= right)
-//     {
-//         int mid = left + (right - left) / 2;
-//         if (S[mid] < n)
-//             left = mid + 1;
-//         else
-//             right = mid -1;
-//     }
-//     return (left);
-// }
+bool    PmergeMe::checkSort(void)
+{
+    for (std::vector<int>::iterator it = _vector.begin(); it != _vector.end() - 1; it++)
+    {
+        int n = *it;
+        for (std::vector<int>::iterator it2 = it + 1; it2 != _vector.end(); it2++)
+            if (n == *it2)
+                return false;
+    }
+    return true;
+}
 
-// std::vector<int>    PmergeMe::generateJacobsthal(int maxIndex) const
-// {
-//     std::vector<int> jacobsthal;
-//     jacobsthal.push_back(0);
-//     jacobsthal.push_back(1);
-    
-//     int next = 1;
-//     while (next <= maxIndex) {
-//         next = jacobsthal[jacobsthal.size() - 1] + 2 * jacobsthal[jacobsthal.size() - 2];
-//         jacobsthal.push_back(next);
-//     }
-//     return jacobsthal;
-// }
-    
-// // std::vector<int>    PmergeMe::generateInsertionOrder(int numElements) const
-// // {
-// //     std::vector<int> order;
-// //     std::vector<int> jacobsthal = generateJacobsthal(numElements);
-// //     for (size_t i = 2; i < jacobsthal.size() && jacobsthal[i] - 2 <= numElements; i++)
-// //     {
-// //         int start = jacobsthal[i];
-// //         int end = jacobsthal[i - 1] + 1;
-// //         for (int j = start; j >= end; j--)
-// //         {
-// //             if (j - 2 <= numElements)
-// //                 order.push_back(j - 2);
-// //         }
-// //     }
-// //     return (order);
-// // }
-
-// std::vector<int> PmergeMe::generateInsertionOrder(int numElements) const {
-//     std::vector<int> order;
-//     std::vector<int> jacobsthal;
-
-//     jacobsthal.push_back(1);
-//     while (jacobsthal.back() < numElements + 1) {
-//         int size = jacobsthal.size();
-//         int next = jacobsthal[size - 1];
-//         if (size == 1)
-//             next = 1;
-//         else
-//             next = jacobsthal[size - 1] + 2 * jacobsthal[size - 2];
-//         jacobsthal.push_back(next);
-//     }
-
-//     std::vector<bool> inserted(numElements, false);
-
-//     // 1. Insérer dans l’ordre inverse Jacobsthal
-//     for (int i = jacobsthal.size() - 1; i >= 0; --i) {
-//         int idx = jacobsthal[i] - 1; // -1 car index de tableau
-//         if (idx < numElements && !inserted[idx]) {
-//             order.push_back(idx);
-//             inserted[idx] = true;
-//         }
-//     }
-
-//     // 2. Compléter avec les indices manquants
-//     for (int i = 0; i < numElements; ++i) {
-//         if (!inserted[i]) {
-//             order.push_back(i);
-//             inserted[i] = true;
-//         }
-//     }
-
-//     return order;
-// }
-
-// void PmergeMe::insert(std::vector<int> const &smaller_elts, std::vector<int> const &big_elts, std::vector<int> &S) const {
-//     std::vector<std::pair<int, int> > pairs;
-
-//     for (size_t i = 0; i < smaller_elts.size(); ++i)
-//         pairs.push_back(std::make_pair(smaller_elts[i], big_elts[i]));
-
-//     // Le premier élément de smaller_elts a déjà été inséré
-//     std::vector<std::pair<int, int> > remainingPairs(pairs.begin() + 1, pairs.end());
-
-//     std::vector<int> insertionOrder = generateInsertionOrder(remainingPairs.size());
-
-//     for (size_t i = 0; i < insertionOrder.size(); ++i) {
-//         int idx = insertionOrder[i];
-//         if (idx >= static_cast<int>(remainingPairs.size()))
-//             continue;
-
-//         int small = remainingPairs[idx].first;
-//         int big = remainingPairs[idx].second;
-
-//         // Chercher où se trouve `big` dans S
-//         int maxPos = 0;
-//         for (size_t j = 0; j < S.size(); ++j) {
-//             if (S[j] == big) {
-//                 maxPos = j;
-//                 break;
-//             }
-//         }
-
-//         int insertPos = binarySearch(S, small, maxPos);
-//         S.insert(S.begin() + insertPos, small);
-//     }
-// }
-
-
-// // void    PmergeMe::insert(std::vector<int> const &smaller_elts, std::vector<int> const &big_elts, std::vector<int> &S) const
-// // {
-// //     std::vector<int> remainingSmall;
-// //     for (size_t i = 1; i < smaller_elts.size(); i++)
-// //         remainingSmall.push_back(smaller_elts[i]);
-// //     std::vector<int> insertionOrder = generateInsertionOrder(remainingSmall.size());
-// //     for (size_t i = 0; i < insertionOrder.size(); i++)
-// //     {
-// //         int idx = insertionOrder[i];
-// //         if (idx < static_cast<int>(remainingSmall.size()))
-// //         {
-// //             int maxPos = 0;
-// //             for (size_t j = 0; j < S.size(); j++) 
-// //             {
-// //                 if (S[j] == big_elts[idx + 1])
-// //                 {
-// //                     maxPos = j;
-// //                     break;
-// //                 }
-// //             }
-// //             int insertPos = binarySearch(S, remainingSmall[idx], maxPos);
-// //             S.insert(S.begin() + insertPos, remainingSmall[idx]);
-// //         }
-// //     }
-// // }
-
-// std::vector<int>    PmergeMe::sort(std::vector<int> &array)
-// {
-//     int n = array.size();
-//     if (n <= 1)
-//         return array;
-//     std::vector<std::pair<int, int> > pairs;
-//     int unpaired = -1;
-//     for (int i = 0; i < n; i += 2) {
-//         if (i + 1 < n)
-//         {
-//             if (array[i] > array[i + 1])
-//                 std::swap(array[i], array[i + 1]);
-//             pairs.push_back(std::make_pair(array[i], array[i + 1]));
-//         }
-//         else
-//             unpaired = array[i];
-//     }
-//     std::vector<int> big_elts;
-//     std::vector<int> smaller_elts;
-//     for (size_t i = 0; i < pairs.size(); i++)
-//     {
-//         big_elts.push_back(pairs[i].second);
-//         smaller_elts.push_back(pairs[i].first);
-//     }
-//     std::vector<int> S = sort(big_elts);
-//     if (!smaller_elts.empty())
-//         S.insert(S.begin(), smaller_elts[0]);
-//     if (smaller_elts.size() > 1)
-//        insert(smaller_elts, big_elts, S);
-//     if (unpaired != -1)
-//     {
-//         int index = binarySearch(S, unpaired, S.size());
-//         S.insert(S.begin() + index, unpaired);
-//     }
-//     return (S);
-// }
-
-int PmergeMe::binarySearch(std::vector<int> const &S, int n, int maxPos) const {
+//algo for vector
+int PmergeMe::binarySearch(std::vector<int> const &S, int n, int maxPos) const
+{
     int left = 0;
     int right = std::min(maxPos, static_cast<int>(S.size()) - 1);
-    while (left <= right) {
+    while (left <= right)
+    {
         int mid = left + (right - left) / 2;
         if (S[mid] < n)
             left = mid + 1;
         else
             right = mid - 1;
     }
-    return left;
+    return (left);
 }
 
-std::vector<int> PmergeMe::generateJacobsthal(int maxIndex) const {
+std::vector<int> PmergeMe::generateJacobsthal(int maxIndex) const
+{
     std::vector<int> jacobsthal;
     jacobsthal.push_back(1);
-    while (jacobsthal.back() < maxIndex + 1) {
+    while (jacobsthal.back() < maxIndex + 1)
+    {
         int size = jacobsthal.size();
         int next = (size == 1) ? 1 : jacobsthal[size - 1] + 2 * jacobsthal[size - 2];
         jacobsthal.push_back(next);
     }
-    return jacobsthal;
+    return (jacobsthal);
 }
 
-std::vector<int> PmergeMe::generateInsertionOrder(int numElements) const {
+std::vector<int> PmergeMe::generateInsertionOrder(int numElements) const
+{
     std::vector<int> order;
     std::vector<int> jacobsthal = generateJacobsthal(numElements);
     std::vector<bool> inserted(numElements, false);
 
-    for (int i = jacobsthal.size() - 1; i >= 0; --i) {
+    for (int i = jacobsthal.size() - 1; i >= 0; --i)
+    {
         int idx = jacobsthal[i] - 1;
-        if (idx < numElements && !inserted[idx]) {
+        if (idx < numElements && !inserted[idx])
+        {
             order.push_back(idx);
             inserted[idx] = true;
         }
     }
-
-    for (int i = 0; i < numElements; ++i) {
-        if (!inserted[i]) {
+    for (int i = 0; i < numElements; i++)
+    {
+        if (!inserted[i])
             order.push_back(i);
-        }
     }
-
-    return order;
+    return (order);
 }
 
-void PmergeMe::insert(std::vector<std::pair<int, int> > const &pairs, std::vector<int> const &, std::vector<int> &result) const {
-    std::vector<std::pair<int, int> > remainingPairs = pairs; // ← ici la correction
-
-    std::vector<int> insertionOrder = generateInsertionOrder(remainingPairs.size());
-
-    for (size_t i = 0; i < insertionOrder.size(); ++i) {
+void PmergeMe::insert(std::vector<std::pair<int, int> > const &pairs, std::vector<int> const &, std::vector<int> &result) const
+{
+    std::vector<int> insertionOrder = generateInsertionOrder(pairs.size());
+    for (size_t i = 0; i < insertionOrder.size(); i++)
+    {
         int idx = insertionOrder[i];
-        if (idx >= static_cast<int>(remainingPairs.size()))
+        if (idx >= static_cast<int>(pairs.size()))
             continue;
-
-        int small = remainingPairs[idx].first;
-        int big = remainingPairs[idx].second;
-
+        int small = pairs[idx].first;
+        int big = pairs[idx].second;
         int maxPos = 0;
-        for (size_t j = 0; j < result.size(); ++j) {
-            if (result[j] == big) {
+        for (size_t j = 0; j < result.size(); j++)
+        {
+            if (result[j] == big)
+            {
                 maxPos = j;
                 break;
             }
         }
-
         int insertPos = binarySearch(result, small, maxPos);
         result.insert(result.begin() + insertPos, small);
     }
 }
 
-
-std::vector<int> PmergeMe::sort(std::vector<int> &array) {
-    int n = array.size();
-    if (n <= 1)
+std::vector<int> PmergeMe::sort(std::vector<int> &array)
+{
+    int size = array.size();
+    if (size <= 1)
         return array;
-
     std::vector<std::pair<int, int> > pairs;
     int unpaired = -1;
-
-    for (int i = 0; i < n; i += 2) {
-        if (i + 1 < n) {
-            int a = array[i], b = array[i + 1];
+    for (int i = 0; i < size; i += 2)
+    {
+        if (i + 1 < size)
+        {
+            int a = array[i];
+            int b = array[i + 1];
             if (a > b)
                 std::swap(a, b);
             pairs.push_back(std::make_pair(a, b));
-        } else {
-            unpaired = array[i];
         }
+        else
+            unpaired = array[i];
     }
-
     std::vector<int> bigs;
-    for (size_t i = 0; i < pairs.size(); ++i)
+    for (size_t i = 0; i < pairs.size(); i++)
         bigs.push_back(pairs[i].second);
-
-    std::vector<int> S = sort(bigs); // Recursive sort on bigs
-
+    std::vector<int> S = sort(bigs);
     std::vector<int> result = S;
-
-    // ⛔ ne pas insérer manuellement pairs[0].first
-    // ✅ insérer tous les petits via insert()
-     if (!pairs.empty())
+    if (!pairs.empty())
         S.insert(S.begin(), pairs[0].first);
     if (!pairs.empty())
         insert(pairs, S, result);
-
-    if (unpaired != -1) {
+    if (unpaired != -1)
+    {
         int pos = binarySearch(result, unpaired, result.size());
         result.insert(result.begin() + pos, unpaired);
     }
-
-    return result;
+    return (result);
 }
+//algo for vector
 
+// //algo for list
+// std::list<int> PmergeMe::generateJacobsthalList(int maxIndex) const
+// {
+//     std::list<int> jacobsthal;
+//     jacobsthal.push_back(1);
+//     while (jacobsthal.back() < maxIndex + 1)
+//     {
+//         int size = jacobsthal.size();
+//         int next;
+//         if (size == 1)
+//             next = 1;
+//         else
+//         {
+//             std::list<int>::iterator it = jacobsthal.end();
+//             --it;
+//             int prev1 = *it;
+//             --it;
+//             int prev2 = *it;
+//             next = prev1 + 2 * prev2;
+//         }
+//         jacobsthal.push_back(next);
+//     }
+//     return (jacobsthal);
+// }
 
+// std::list<int> PmergeMe::generateInsertionOrderList(int numElements) const
+// {
+//     std::list<int> order;
+//     std::list<int> jacobsthal = generateJacobsthalList(numElements);
+//     std::list<bool> inserted(numElements, false);
+
+//     if (!jacobsthal.empty())
+//     {
+//         std::list<int>::iterator it = --jacobsthal.end();
+//         while (1)
+//         {
+//             int idx = *it - 1;
+//             if (idx < numElements && !inserted[idx])
+//             {
+//                 order.push_back(idx);
+//                 // inserted[idx] = true;
+//             }
+//             if (it == jacobsthal.begin())
+//                 break;
+//             --it;
+//         }
+//     }
+//     return (order);
+//     // for (int i = jacobsthal.size() - 1; i >= 0; --i)
+//     // {
+//     //     int idx = jacobsthal[i] - 1;
+//     //     if (idx < numElements && !inserted[idx])
+//     //     {
+//     //         order.push_back(idx);
+//     //         inserted[idx] = true;
+//     //     }
+//     // }
+//     // for (int i = 0; i < numElements; i++)
+//     // {
+//     //     if (!inserted[i])
+//     //         order.push_back(i);
+//     // }
+//     // return (order);
+// }
+
+// int PmergeMe::binarySearch(std::list<int> const &S, int n, int maxPos) const
+// {
+//     int left = 0;
+//     int right = std::min(maxPos, static_cast<int>(S.size()) - 1);
+//     while (left <= right)
+//     {
+//         int mid = left + (right - left) / 2;
+//         if (S[mid] < n)
+//             left = mid + 1;
+//         else
+//             right = mid - 1;
+//     }
+//     return (left);
+// }
+
+// void PmergeMe::insert(std::list<std::pair<int, int> > &pairs, std::list<int> const &, std::list<int> &result) const
+// {
+//     std::list<int> insertionOrder = generateInsertionOrderList(pairs.size());
+//     for (size_t i = 0; i < insertionOrder.size(); ++i)
+//     for (std::list<int>::iterator it = insertionOrder.begin(); it != insertionOrder.end(); it++)
+//     {
+//         int idx = *it;
+//         if (idx >= static_cast<int>(pairs.size()))
+//             continue;
+//         std::list<std::pair<int, int> >::iterator it_pair = pairs.begin();
+//         for (int i = 0; i < idx && it_pair != pairs.end(); ++i, ++it_pair){
+//         }
+//         int small = it_pair->first;
+//         int big = it_pair->second;
+//         int maxPos = 0;
+//         for (std::list<int>::iterator it = result.begin(); it != result.end(); it++)
+//         {
+//             if (*it == big)
+//             {
+//                 maxPos = *it;
+//                 break;
+//             }
+//         }
+//         int insertPos = binarySearch(result, small, maxPos);
+//         result.insert(result.begin() + insertPos, small);
+//     }
+// }
+
+// std::list<int>  PmergeMe::sort(std::list<int> &array)
+// {
+//     int size = array.size();
+//     if (size <= 1)
+//         return array;
+//     std::list<std::pair<int, int> > pairs;
+//     int unpaired = -1;
+//     for (std::list<int>::iterator it = array.begin(); it != array.end(); std::advance(it, 2))
+//     {
+//         std::list<int>::iterator next = it;
+//         ++next;
+//         if (next != array.end())
+//         {
+//             int a = *it;
+//             int b = *next;
+//             if (a > b)
+//                 std::swap(a, b);
+//             pairs.push_back(std::make_pair(a, b));
+//         }
+//         else
+//             unpaired = *it;
+//     }
+//     std::list<int> bigs;
+//     for (std::list<std::pair<int, int> >::iterator it = pairs.begin(); it != pairs.end(); it++)
+//         bigs.push_back(it->second);
+//     std::list<int> S = sort(bigs);
+//     std::list<int> result = S;
+//     if (!pairs.empty())
+//         S.insert(S.begin(), pairs[0].first);
+//     if (!pairs.empty())
+//         insert(pairs, S, result);
+//     if (unpaired != -1)
+//     {
+//         int pos = binarySearch(result, unpaired, result.size());
+//         result.insert(result.begin() + pos, unpaired);
+//     }
+//     return (result);
+// }
+//algo for list
 
 bool    PmergeMe::onlyDigit(const char *input) const
 {
@@ -356,4 +336,3 @@ bool    PmergeMe::onlyDigit(const char *input) const
     }
     return true;
 }
-
